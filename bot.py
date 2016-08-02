2e4b65082316b0f470150c71324fad1eb40b49ee
rohan <rohan.dhaimade@gmail.com>
2016-08-02 09:27:33 -0700

    host nad port

--- bot.py
+++ bot.py
@@ -256,4 +256,6 @@ def get_map():
 if __name__ == '__main__':
 	spawn_workers(workers)
 	args = get_args()
+	port = int(os.environ.get('PORT', 5000))
+	host = '0.0.0.0'
 	app.run(debug=True, threaded=True, host=args.host, port=args.port)
